dcc37d4ba1ab8d8ae8a2213656cfac58287656f0
Amir Ossanloo <6134758+AmirOssanloo@users.noreply.github.com>
2020-10-17 15:06:13 +0200

    Initial commit

---
+++ 001-hello-world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string a = "Hello";
+      string b = "World";
+      Console.WriteLine("{0} {1}!", a, b);
+    }
+  }
+}
