f5b886448f7a04cd7e8b8f6287eeb97f8aca6123
Amir Ossanloo <6134758+AmirOssanloo@users.noreply.github.com>
2020-10-18 19:03:49 +0200

    Added first project

---
+++ 001-divisible-by-three/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int current = 0;
+      int found = 0;
+      int target = 100;
+
+      while (current <= target)
+      {
+        if (current % 3 == 0)
+        {
+          found++;
+        }
+
+        current++;
+      }
+
+      Console.WriteLine(found);
+    }
+  }
+}
