e5658eefe85c313f37f8e2da4679f3d96675bafe
Amir Ossanloo <6134758+AmirOssanloo@users.noreply.github.com>
2020-10-18 19:20:05 +0200

    Added ask-for-input

---
+++ 002-ask-for-input/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _002_ask_for_input
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int total = 0;
+      string input;
+
+      do
+      {
+        input = Console.ReadLine();
+
+        if (Int32.TryParse(input, out _))
+        {
+          total += Convert.ToInt32(input);
+        };
+      } while (input != "ok");
+
+
+      Console.WriteLine("The total is {0}!", total);
+    }
+  }
+}
