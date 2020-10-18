3724175aa69139e69bd2d4ee07beee978a31ce78
Amir Ossanloo <6134758+AmirOssanloo@users.noreply.github.com>
2020-10-18 16:29:22 +0200

    Worked on classes

---
+++ 003-classes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Classes;
+
+namespace Classes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var a = Calculator.Add(1, 5);
+      var b = Calculator.Subtract(a, 5);
+      var c = Calculator.Multiply(b, 3);
+
+      Console.WriteLine("The result is {0}", c);
+    }
+  }
+}
